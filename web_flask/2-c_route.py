09c49ca618cb1e0028301b417bff05dc53104620
Victortimbwa <victortimbwa@gmail.com>
2024-06-05 04:19:18 +0300

    A script that starts a Flask web application

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+""" This script starts a Flask web application with specific routes."""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Display 'Hello HBNB!' on the main route.
+    """
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """
+    Display 'HBNB' on the /hbnb route.
+    """
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_text(text):
+    """
+    Display 'C ' followed by the value of the text variable.
+    Replace underscores with spaces in the text.
+    """
+    return "C " + text.replace("_", " ")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
