a1cdfddadbb84167d8df76e3e5da37fd19a09514
Victortimbwa <victortimbwa@gmail.com>
2024-06-05 04:52:36 +0300

    Flask changes

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,45 +1,33 @@
 #!/usr/bin/python3
 """This script starts a Flask web application."""
 
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Display "Hello HBNB!" on the root route."""
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def display_hbnb():
     """Display "HBNB" on /hbnb route."""
     return 'HBNB'
 
 
-@app.route('/c/<text>')
-def display_c_text(text):
+@app.route('/c/<text>', strict_slashes=False)
+def display_c_text(text='is cool'):
     """Display 'C ' followed by the value of the text variable."""
     return 'C {}'.format(text.replace('_', ' '))
 
 
-@app.route('/python/<text>')
-def display_python_text(text):
+@app.route('/python/<text>', strict_slashes=False)
+def display_python_text(text='is cool'):
     """Display 'Python ' followed by the value of the text variable."""
     return 'Python {}'.format(text.replace('_', ' '))
-
-
-@app.route('/number/<int:n>')
-def display_number(n):
-    """Display 'n is a number' only if n is an integer."""
-    return '{} is a number'.format(n)
-
-
-@app.route('/number_template/<int:n>')
-def display_number_template(n):
-    """Display an HTML page if n is an integer."""
-    return render_template('number.html', n=n)
 
 
 if __name__ == '__main__':
