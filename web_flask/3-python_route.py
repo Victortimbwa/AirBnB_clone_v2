ec5814dacf81ab4ddec1b0412040aaa4a1997535
Victortimbwa <victortimbwa@gmail.com>
2024-06-05 04:32:42 +0300

    Corrections

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -8,20 +8,28 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_hbnb():
-    """Display "Hello HBNB!" on the root route."""
+    """Display 'Hello HBNB!' on the root route."""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb')
 def display_hbnb():
-    """Display "HBNB" on /hbnb route."""
+    """Display 'HBNB' on /hbnb route."""
     return 'HBNB'
 
 
+@app.route('/c/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/c/<text>', strict_slashes=False)
 def display_c_text(text):
     """Display 'C ' followed by the value of the text variable."""
-    return 'C {}'.format(text.replace('_', ' ')
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_python_text(text):
+    """Display 'Python ' followed by the value of the text variable."""
+    return 'Python {}'.format(text.replace('_', ' '))
 
 
 if __name__ == '__main__':
